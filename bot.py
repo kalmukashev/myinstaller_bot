b4c774226a7c9b6a6872cf2f2bff64cf98493b41
kalmukashev <77501241+kalmukashev@users.noreply.github.com>
2021-01-19 22:58:29 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,18 +26,20 @@ def create_keyboard2():
 	vk_btn = types.InlineKeyboardButton(text = "Вконтакте", url="https://vk.com")
 	insta_btn = types.InlineKeyboardButton(text = "Instagram", url="https://www.instagram.com")
 	faceb_btn = types.InlineKeyboardButton(text = "Facebook", url="https://www.facebook.com")
+	twitter_btn = types.InlineKeyboardButton(text = "Twitter", url="https://twitter.com")
 	yout_btn = types.InlineKeyboardButton(text = "Youtube", url="https://www.youtube.com")
 	twitch_btn = types.InlineKeyboardButton(text = "Twitch", url="https://www.twitch.tv")
 	keyboard.add(vk_btn)
 	keyboard.add(insta_btn)
 	keyboard.add(faceb_btn)
+	keyboard.add(twitter_btn)
 	keyboard.add(yout_btn)
 	keyboard.add(twitch_btn)
 	return keyboard
 
 def create_keyboard3():
 	keyboard = types.InlineKeyboardMarkup()
-	acmp_btn = types.InlineKeyboardButton(text = "Школа Программиста", url="https://acmp.ru")
+	acmp_btn = types.InlineKeyboardButton(text = "Школа Программиста(acmp)", url="https://acmp.ru")
 	stepik_btn = types.InlineKeyboardButton(text = "Stepik", url="https://stepik.org")
 	keyboard.add(acmp_btn)
 	keyboard.add(stepik_btn)
@@ -56,6 +58,6 @@ def get_download(message):
 	if message.text.lower() not in hey and message.text != 'Скачать' and message.text != 'скачать' and message.text != '/help' and message.text != 'Ссылки' and message.text != 'ссылки' and message.text != 'Задачки' and message.text != 'задачки':
 		bot.send_message(message.chat.id, "У вас ошибка! Введи /help и пиши, то что там написано, спасибо за понимание.")
 	if message.text == '/help':
-		bot.send_message(message.chat.id, "1 Команда: Напиши Привет\n2 Команда: Напиши Скачать или скачать\n3 Команда: Напиши Ссылки или ссылки\n4 Команда: Напиши Задачи или задачи")
+		bot.send_message(message.chat.id, "1 Команда: Напиши Привет или привет\n2 Команда: Напиши Скачать или скачать\n3 Команда: Напиши Ссылки или ссылки\n4 Команда: Напиши Задачки или задачки\n")
 
 bot.polling(none_stop=True, interval = 0)
